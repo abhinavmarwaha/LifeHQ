45831713cfc1ab6652576de0370d1f77628a1d9a
Abhinav Marwaha <abhinavmarwaha.bt19cse@pec.edu.in>
2021-10-25 01:04:52 +0530

    demo test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:lifehq/main.dart';
@@ -6,10 +5,6 @@ import 'package:lifehq/main.dart';
 void main() {
   testWidgets('demo', (WidgetTester tester) async {
     await tester.pumpWidget(MyApp());
-
-    await tester.tap(find.byIcon(Icons.arrow_forward_ios));
-    await tester.pump();
-
-    expect('', '');
+    expect(true, true);
   });
 }
